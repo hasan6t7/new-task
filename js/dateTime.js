498dfcb4d5ceb483e2d3f117cbf790d45c68117b
hasan6t7 <hasan.hasanujjaman4368@gmail.com>
2025-03-02 00:40:31 +0600

    some changes

--- js/dateTime.js
+++ js/dateTime.js
@@ -1,3 +1,33 @@
+function updateDate() {
+  let today = new Date();
+  let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let monthsOfYear = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let weekday = daysOfWeek[today.getDay()];
+  let month = monthsOfYear[today.getMonth()];
+  let day = today.getDate();
+  let year = today.getFullYear();
+  let formattedDate =
+    weekday + " " + month + " " + (day < 10 ? "0" + day : day) + ", " + year;
+  
+document.getElementById("day-date").innerText = formattedDate;
+
+}
+updateDate();
+
 const now = new Date();
 let hours = now.getHours();
 const minutes = String(now.getMinutes()).padStart(2, "0");
